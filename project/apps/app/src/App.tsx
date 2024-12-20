4ca04ad64f02f69de5a8cf1049d0b8d0f17815d5
neelamkokane <kokaneneelam@gmail.com>
2024-12-20 19:14:58 +0530

    'task-1'

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,12 +1,24 @@
-import { List } from 'ui'
+import { useEffect, useState } from 'react'
+import List from 'ui/components/List'
 
 const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
 
 const App = () => {
+
+  const [data, setData] = useState<any>([])
+
+  useEffect(() => {
+    let fetchRes = fetch(api)
+    fetchRes.then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      setData(data.results)})
+  },[])
+
   return (
   <>
     <h1>Pokemon list:</h1>
-    <List />
+    <List/>
   </>
   )
 }
